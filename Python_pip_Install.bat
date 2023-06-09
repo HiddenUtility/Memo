a19c01b9f8582ecc16aa6911a9407ab8d404345f
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-06-09 21:50:08 +0900

    create

---
+++ Python_pip_Install.bat
@@ -0,0 +1,36 @@
+pip install -U pip
+
+
+pip install pip-review
+pip install tqdm
+pip install joblib
+pip install pandas
+pip install numpy
+pip install scipy
+pip install xlrd
+pip install XlsxWriter
+pip install matplotlib
+pip install japanize-matplotlib
+pip install Pillow
+pip install opencv-python
+pip install folium
+pip install plotly
+pip install wordcloud
+pip install requests
+pip install beautifulsoup4
+pip install lxml
+pip install selenium
+pip install Flask
+pip install Flask-Bootstrap4
+pip install Django
+pip install PyMySQL
+pip install psycopg2
+pip install PyInstaller
+pip install h5py
+pip install spyder
+
+rem ツイッターAPIを扱うパッケージです。
+pip install tweepy
+
+rem 時系列データの統計処理とか。
+pip install tslearn
